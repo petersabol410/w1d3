e22b6b209a35336ddb1a29699dc86e1472f48bb8
petersabol410 <peter.sabol8@gmail.com>
2018-01-17 20:58:45 +0000

    Counting characters

---
+++ counting-characters.js
@@ -0,0 +1,18 @@
+var input = process.argv.slice(2);
+var noSpaces = input.join("");
+//console.log(noSpaces[0]);
+
+var countLetters = function(noSpaces) {
+  var letterObject = {};
+
+  for (var i = 0; i < noSpaces.length; i++) {
+    if (noSpaces[i] in letterObject) {
+      letterObject[noSpaces[i]] += 1;
+    } else {
+      letterObject[noSpaces[i]] = 1;
+    }
+  }
+  return letterObject;
+};
+
+console.log(countLetters(noSpaces));
